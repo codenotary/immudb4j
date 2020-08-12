0e1a8ac0f26c2b0b6924a9bdc7afbe0ef303a59e
Jeronimo Irazabal <jeronimo.irazabal@gmail.com>
2020-08-12 09:22:19 -0300

    download darwin or linux immudb binary

--- immudb/start.sh
+++ immudb/start.sh
@@ -6,7 +6,13 @@ if [ ! -e immudb ]
 then
   echo "Downloading immudb..."
 
-  curl -o immudb -L https://github.com/vchain-us/immudb/releases/download/v0.7.0/immudb-v0.7.0-darwin-amd64
+  if [[ "$OSTYPE" == "linux"* ]]; then
+    URL=https://github.com/vchain-us/immudb/releases/download/v0.7.0/immudb-v0.7.0-linux-amd64
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    URL=https://github.com/vchain-us/immudb/releases/download/v0.7.0/immudb-v0.7.0-darwin-amd64
+  fi
+
+  curl -o immudb -L $URL
   chmod +x immudb
 
   echo "Downloaded"
