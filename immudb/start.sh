a2c044c4cd68dce1016423bbcfa658a66c0987ec
Jeronimo Irazabal <jeronimo.irazabal@gmail.com>
2020-08-12 09:44:31 -0300

    fix immudb downloading in linux os

--- immudb/start.sh
+++ immudb/start.sh
@@ -6,13 +6,14 @@ if [ ! -e immudb ]
 then
   echo "Downloading immudb..."
 
-  if [[ "$OSTYPE" == "linux"* ]]; then
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     URL=https://github.com/vchain-us/immudb/releases/download/v0.7.0/immudb-v0.7.0-linux-amd64
+    wget -O immudb $URL
   elif [[ "$OSTYPE" == "darwin"* ]]; then
     URL=https://github.com/vchain-us/immudb/releases/download/v0.7.0/immudb-v0.7.0-darwin-amd64
+    curl -o immudb -L $URL
   fi
 
-  curl -o immudb -L $URL
   chmod +x immudb
 
   echo "Downloaded"
