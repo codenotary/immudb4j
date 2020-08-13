36cd357711582def3385611b05403e3f3c15a272
Jeronimo Irazabal <jeronimo.irazabal@gmail.com>
2020-08-13 11:42:26 -0300

    util: add clean script to remove immudb data and roots folder

---
+++ immudb/clean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+if [ -e pid ]
+then
+  sh ./stop.sh
+
+  # give some time to immudb to gracefully shutdown
+  sleep 1
+fi
+
+rm -rf data
+rm -rf roots
+
+echo "immudb4j data and roots folders removed."
