8f8232ac52c16df831088b85424a1db7f8fcb0ac
Jeronimo Irazabal <jeronimo.irazabal@gmail.com>
2023-10-05 09:44:25 +0200

    chore: bump immudb to 1.5.0
    Signed-off-by: Jeronimo Irazabal <jeronimo.irazabal@gmail.com>

--- immudb/start.sh
+++ immudb/start.sh
@@ -13,10 +13,10 @@ then
   echo "Downloading immudb..."
 
   if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    URL=https://github.com/vchain-us/immudb/releases/download/v1.4.0/immudb-v1.4.0-linux-amd64
+    URL=https://github.com/vchain-us/immudb/releases/download/v1.5.0/immudb-v1.5.0-linux-amd64
     wget -O immudb $URL
   elif [[ "$OSTYPE" == "darwin"* ]]; then
-    URL=https://github.com/vchain-us/immudb/releases/download/v1.4.0/immudb-v1.4.0-darwin-amd64
+    URL=https://github.com/vchain-us/immudb/releases/download/v1.5.0/immudb-v1.5.0-darwin-amd64
     curl -o immudb -L $URL
   fi
 
