4b0757d86728961eb71d83f3709f93c0514dadec
Stefano Scafiti <stefano.scafiti96@gmail.com>
2024-05-21 15:25:08 +0200

    Add acceptStream

--- src/main/java/io/codenotary/immudb4j/ImmuClient.java
+++ src/main/java/io/codenotary/immudb4j/ImmuClient.java
@@ -369,6 +369,7 @@ public class ImmuClient {
         final ImmudbProto.SQLQueryRequest req = ImmudbProto.SQLQueryRequest.newBuilder()
                 .setSql(stmt)
                 .addAllParams(sqlEncodeParams(params))
+                .setAcceptStream(true)
                 .build();
 
         return new SQLQueryResult(blockingStub.txSQLQuery(req));
